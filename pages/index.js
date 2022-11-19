ee57d8e76e0548a0c37bbb5a38f00b65ec55de78
dgregpm <dgregpm@gmail.com>
2022-11-19 12:07:20 -0500

    Removed unused imports

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import React from 'react'
 import Header from '../components/Header'
 import Banner from '../components/Banner'
 
