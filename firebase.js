27388f9d9c894d51f00c7277eff9158dca969a96
dgregpm <dgregpm@gmail.com>
2022-11-20 07:16:51 -0500

    Added firebase config

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDQr8Z6lnm34kVGno1cPyFU_ZYTwVL3pKY",
+    authDomain: "ecommerce-nextjs-d3bd8.firebaseapp.com",
+    projectId: "ecommerce-nextjs-d3bd8",
+    storageBucket: "ecommerce-nextjs-d3bd8.appspot.com",
+    messagingSenderId: "615880981899",
+    appId: "1:615880981899:web:d10e9e3c7b1f5bf2e6c38e",
+    measurementId: "G-HG5ZMQKRHV"
+  };
